cea1a7bdc223b348eb498120636c1bc79a4fe45f
andrewdiyi <44893972+andrewdiyi@users.noreply.github.com>
2025-09-05 14:54:51 +0800

    Update Subconverter.vue
    Add self-hosted sub api

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,12 +338,13 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "自用后端": "https://apisub.likly.top",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
+          "nameless13提供": "https://www.nameless13.com",
+          "subconverter作者提供": "https://sub.xeton.dev",
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
@@ -1310,3 +1311,4 @@ export default {
   }
 };
 </script>
+
